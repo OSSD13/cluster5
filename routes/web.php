8c4467b87de4be79bdd6d6d553824838a6dd0adf
tawan475 <43491902+tawan475@users.noreply.github.com>
2025-03-06 12:04:18 +0000

    Remove jobs migration and add registration and user display functionality

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+Route::get('/register', [RegisterController::class, 'create']);
+Route::get('/displayLogin', [RegisterController::class, 'displayLogin']);
