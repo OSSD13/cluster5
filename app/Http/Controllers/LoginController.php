f29a16bb4300c5d9d7ca25d88685c39398186fa8
Nitiwadee-Pang <pang.nitiwadee@gmail.com>
2025-03-13 11:24:31 +0700

    OSSD

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,27 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Models\User;
-
-use Illuminate\Support\Facades\Hash;
-
-
 class LoginController extends Controller
 {
-    function view() {
-        return view('test _login');
-    }
-
-    function login(Request $req){
-        $user = User::where('email', $req->email)->first();
-        if($user != null && Hash::check($req->password, $user->password)){
-            $req->session()->put('user', $user);
-            return redirect('/convert-link');
-        
-        }
-        else{
-            $req->session()->flash('error', 'ข้อมูลการเข้าสู่ระบบไม่ถูกต้อง');
-                    return redirect('/login');
-        }
+    function login()
+    {
+        return view('login');
     }
 }
