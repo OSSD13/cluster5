19ab877bfdbfa15515f1b669c2bc7ad06aa9e9c3
tawan475 <43491902+tawan475@users.noreply.github.com>
2025-03-13 06:53:35 +0000

    Add Google authentication with Socialite and create login view

---
+++ resources/views/displayTestLogin.blade.php
@@ -0,0 +1,35 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div class="container">
+        <h1>Login</h1>
+        @if (session('error'))
+            <div class="alert alert-danger">
+                {{ session('error') }}
+            </div>
+        @endif
+        {{-- <form action="{{ route('login') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" name="password" id="password" class="form-control" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form> --}}
+        <hr>
+        <a href="{{ route('google-auth') }}" class="btn btn-danger">Login with Google</a>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
