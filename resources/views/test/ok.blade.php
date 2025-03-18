1478690e3dd594af18371c438937ba1ac43b5a82
jeng <189732922+TorlapSayHi@users.noreply.github.com>
2025-03-18 15:48:34 +0000

    Add new component layout and route for test view

--- resources/views/test/ok.blade.php
+++ resources/views/test/ok.blade.php
@@ -18,22 +18,55 @@
         @endif
     </head>
     <!-- component -->
-<link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
-
-<style>
-.fortnite-btn {
-  	background: linear-gradient(#fefb72, #fefca3);
-	font-family: 'Luckiest Guy';
-}
+<<!-- component -->
+<!-- This is an example component -->
+<div class="flex flex-col">
+  <div>
+      <button type="button" class="bg-blue-600 text-white p-2 rounded  leading-none flex items-center">
+          Notifications <span class="bg-white p-1 rounded text-blue-600 text-xs ml-2">4</span>
+      </button>
+  </div>
+  <div class="mt-8 flex">
+    <button type="button" class="mr-2 bg-blue-600 text-white p-2 rounded  leading-none flex items-center">
+      	New
+    </button>
+    <button type="button" class="mr-2 bg-red-600 text-white p-2 rounded  leading-none flex items-center">
+      	New
+    </button>
+    
+    <button type="button" class="mr-2 bg-orange-600 text-white p-2 rounded  leading-none flex items-center">
+      	New
+    </button>
+    
+    <button type="button" class="mr-2 bg-green-600 text-white p-2 rounded  leading-none flex items-center">
+      	New
+    </button>
+    
+    <button type="button" class="bg-white text-black p-2 rounded  leading-none flex items-center">
+      	New
+    </button>
+  </div>
   
-.fortnite-btn-inner {
-  	background: linear-gradient(#ede801, #fefb72);
-	transform: skew(-5deg);
-	color: #343F65;
-}
-</style>
-
-<button class="fortnite-btn flex items-center justify-center h-32 w-64">
-	<span class="fortnite-btn-inner p-3 pt-5 w-11/12 text-5xl truncate">Play</span>
-</button>
+  
+  <div class="mt-8 flex">
+    <button type="button" class="rounded-full px-4 mr-2 bg-blue-600 text-white p-2 rounded  leading-none flex items-center">
+      	New
+    </button>
+    <button type="button" class="rounded-full px-4 mr-2 bg-red-600 text-white p-2 rounded  leading-none flex items-center">
+      	New
+    </button>
+    
+    <button type="button" class="rounded-full px-4 mr-2 bg-orange-600 text-white p-2 rounded  leading-none flex items-center">
+      	New
+    </button>
+    
+    <button type="button" class="rounded-full px-4 mr-2 bg-green-600 text-white p-2 rounded  leading-none flex items-center">
+      	New
+    </button>
+    
+    <button type="button" class="rounded-full px-4 bg-white text-black p-2 rounded  leading-none flex items-center">
+      	New
+    </button>
+  </div>
+</div>
 </html>
