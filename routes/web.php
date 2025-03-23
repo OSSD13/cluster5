f9d4b34905dc873e41af4a4469c508ee001ae775
Pang <pang,nitiwadee@gmail.com>
2025-03-23 16:15:31 +0000

    fix(login):แก้ไขเลย์เอ้า2

--- routes/web.php
+++ routes/web.php
@@ -58,4 +58,6 @@ Route::middleware([CheckLogin::class])->group(function () {
     // Google Maps URL conversion routes
     Route::get('/convert-link', [AnotherController::class, 'showForm']);
     Route::post('/convert-url', [AnotherController::class, 'handleConversion'])->name('handleConversion');
+
+    
 });
