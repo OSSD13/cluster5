ec841efe8f45a27bb3156149888ca1408c10d31f
mos <Raverojsonthi47@gmail.com>
2025-04-07 06:34:39 +0000

    refactor: implement insert functionality for POI and enhance form validation

--- resources/views/poi/create.blade.php
+++ resources/views/poi/create.blade.php
@@ -10,6 +10,8 @@
         }
     </style>
     <form method="POST" action="{{ route('poi.insert') }}" name="poiForm">
+
+    
         @csrf
         <div class="max-w-md mx-auto bg-white shadow-lg rounded-lg p-6">
             <h2 class="text-2xl font-bold text-gray-800 mb-4">POI เพิ่มสถานที่</h2>
@@ -18,76 +20,63 @@
             <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mb-3 " placeholder="Link Google">
 
             <label class="block text-sm text-gray-600">ละติจูด</label>
-            <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mb-3" placeholder="ละติจูด"
-                name="latitude" value="{{ old('latitude') }}" onkeyup="checkForm()">
-
+            <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mb-1" placeholder="ละติจูด"
+                name="latitude" value="{{ old('latitude') }}" onkeyup="checkForm()" id="latitude">
+            <span id="latitude-error" class="text-red-500 text-sm hidden">กรุณาใส่ละติจูดให้ถูกต้อง (เช่น 13.7563)</span>
 
             <label class="block text-sm text-gray-600">ลองจิจูด</label>
-            <input type="text"
-                class="w-full p-2 border border-gray-300 rounded-lg mb-3 @error('longitude') error-input-style @enderror"
-                placeholder="ลองจิจูด" name="longitude" value="{{ old('longitude') }}">
-            @error('longitude')
-                <div class="text-red-500 text-sm mb-2">{{ $message }}</div>
-            @enderror
+            <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mb-1" placeholder="ลองจิจูด"
+                name="longitude" value="{{ old('longitude') }}" onkeyup="checkForm()" id="longitude">
+            <span id="longitude-error" class="text-red-500 text-sm hidden">กรุณาใส่ลองจิจูดให้ถูกต้อง (เช่น 100.5018)</span>
+
+
 
             <div class="w-full h-48 bg-gray-200 rounded-lg mb-3">
                 <img src="your-map-image-url.png" alt="Map" class="w-full h-full object-cover rounded-lg">
             </div>
 
             <label class="block text-sm text-gray-600">รหัสไปรษณีย์</label>
-            <input type="text"
-                class="w-full p-2 border border-gray-300 rounded-lg mb-3 @error('postal_code') error-input-style @enderror"
-                placeholder="รหัสไปรษณีย์" name="postal_code" value="{{ old('postal_code') }}">
-            @error('postal_code')
-                <div class="text-red-500 text-sm mb-2">{{ $message }}</div>
-            @enderror
+            <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mb-3 " placeholder="รหัสไปรษณีย์"
+                name="postal_code" value="{{ old('postal_code') }}" onkeyup="checkForm()">
+            <span id="postal-error" class="text-red-500 text-sm hidden">กรุณาใส่รหัสไปรษณีย์ให้ถูกต้อง (5 หลัก)</span>
 
             <label class="block text-sm text-gray-600">จังหวัด</label>
-            <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mb-3 @error('province') error-input-style
-            @enderror" placeholder="จังหวัด" name="province" value="{{ old('province') }}">
-            @error('province')
-                <div class="text-red-500 text-sm mb-2">{{ $message }}</div>
-            @enderror
+            <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mb-3 " placeholder="จังหวัด"
+                name="province" value="{{ old('province') }}" onkeyup="checkForm()">
+
 
             <label class="block text-sm text-gray-600">อำเภอ</label>
-            <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mb-3 @error('district') error-input-style
-            @enderror" placeholder="อำเภอ" name="district" value="{{ old('district') }}">
-            @error('district')
-                <div class="text-red-500 text-sm mb-2">{{ $message }}</div>
-            @enderror
+            <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mb-3 " placeholder="อำเภอ"
+                name="district" value="{{ old('district') }}" onkeyup="checkForm()">
+            <span id="province-error" class="text-red-500 text-sm hidden">กรุณากรอกจังหวัดให้ถูกต้อง</span>
+
 
             <label class="block text-sm text-gray-600">ตำบล</label>
-            <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mb-3 @error('sub_district') error-input-style
-            @enderror" placeholder="ตำบล" name="sub_district" value="{{ old('sub_district') }}">
-            @error('sub_district')
-                <div class="text-red-500 text-sm mb-2">{{ $message }}</div>
-            @enderror
+            <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mb-3" placeholder="ตำบล"
+                name="sub_district" value="{{ old('sub_district') }}" onkeyup="checkForm()">
+
 
             <label class="block text-sm text-gray-600">ที่อยู่</label>
-            <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mb-3 @error('address') error-input-style
-            @enderror" placeholder="ที่อยู่" name="address" value="{{ old('address') }}">
-            @error('address')
-                <div class="text-red-500 text-sm mb-2">{{ $message }}</div>
-            @enderror
+            <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mb-3 " placeholder="ที่อยู่"
+                name="address" value="{{ old('address') }}" onkeyup="checkForm()">
+
 
 
             <label class="block text-sm text-gray-600">ชื่อ</label>
-            <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mb-3 @error('name') error-input-style
-            @enderror" placeholder="ชื่อ" name="name" value="{{ old('name') }}">
-            @error('name')
-                <div class="text-red-500 text-sm mb-2">{{ $message }}</div>
-            @enderror
+            <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mb-3 " placeholder="ชื่อ" name="name"
+                value="{{ old('name') }}" onkeyup="checkForm()">
+
 
             <label class="block text-sm text-gray-600">ประเภท</label>
             <select class="w-full p-2 border border-gray-300 rounded-lg mb-3 @error('type') error-input-style @enderror"
-                name="type">
-                <option class="hidden " value="" disabled {{ old('type') == '' ? 'selected' : '' }}>เลือกประเภทสถานที่
+                name="type" onchange="checkForm()">
+                <option class="hidden" value="" disabled {{ old('type') == '' ? 'selected' : '' }}>เลือกประเภทสถานที่
                 </option>
                 <option value="1" {{ old('type') == '1' ? 'selected' : '' }}>1</option>
             </select>
-            @error('type')
-                <div class="text-red-500 text-sm mb-2">{{ $message }}</div>
-            @enderror
+
+
+
 
 
             <div class="flex justify-between">
@@ -101,24 +90,91 @@
         </div>
 
         <script>
-            function checkForm() {
-                // ดึงค่าของฟิลด์ latitude
-                var latitude = document.forms['poiForm'].elements['latitude'].value;
+            function isValidCoordinate(value, type = "lat") {
+                const floatVal = parseFloat(value);
+                if (isNaN(floatVal)) return false;
+                return (type === "lat") ? floatVal >= -90 && floatVal <= 90 : floatVal >= -180 && floatVal <= 180;
+            }
 
-                // ตรวจสอบว่าฟิลด์ latitude ไม่ว่าง
-                if (latitude.trim() !== "") {
-                    // เปิดใช้งานปุ่ม
+            function isValidPostalCode(value) {
+                return /^\d{5}$/.test(value);
+            }
+
+            function isValidText(value) {
+                // ตรวจสอบว่ามีเฉพาะตัวอักษรไทย อังกฤษ และเว้นวรรค (ถ้าอยากละเอียดกว่านี้ก็แจ้งได้)
+                return /^[\u0E00-\u0E7Fa-zA-Z\s]+$/.test(value);
+            }
+
+            function checkForm() {
+                var form = document.forms['poiForm'];
+
+                var latitude = form.elements['latitude'].value.trim();
+                var longitude = form.elements['longitude'].value.trim();
+                var postal_code = form.elements['postal_code'].value.trim();
+                var province = form.elements['province'].value.trim();
+                var district = form.elements['district'].value.trim();
+                var sub_district = form.elements['sub_district'].value.trim();
+                var address = form.elements['address'].value.trim();
+                var name = form.elements['name'].value.trim();
+                var type = form.elements['type'].value.trim();
+
+                // ตรวจค่าที่ต้องมีรูปแบบ
+                var isLatValid = isValidCoordinate(latitude, "lat");
+                var isLonValid = isValidCoordinate(longitude, "lon");
+                var isPostalValid = isValidPostalCode(postal_code);
+                var isProvinceValid = isValidText(province);
+                var isDistrictValid = isValidText(district);
+                var isSubDistrictValid = isValidText(sub_district);
+                var isAddressValid = address !== "";
+                var isNameValid = isValidText(name);
+
+                // Error message + red border toggle
+                toggleError("latitude", isLatValid);
+                toggleError("longitude", isLonValid);
+                toggleError("postal_code", isPostalValid, "postal-error");
+                toggleError("province", isProvinceValid, "province-error");
+                toggleError("district", isDistrictValid, "district-error");
+                toggleError("sub_district", isSubDistrictValid, "sub-district-error");
+                toggleError("address", isAddressValid, "address-error");
+                toggleError("name", isNameValid, "name-error");
+
+                // ตรวจสอบทั้งหมดก่อนเปิดปุ่ม
+                if (
+                    isLatValid &&
+                    isLonValid &&
+                    isPostalValid &&
+                    isProvinceValid &&
+                    isDistrictValid &&
+                    isSubDistrictValid &&
+                    isAddressValid &&
+                    isNameValid &&
+                    type !== ""
+                ) {
                     document.getElementById("saveButton").disabled = false;
                     document.getElementById("saveButton").classList.remove("cursor-not-allowed", "bg-gray-500");
                     document.getElementById("saveButton").classList.add("bg-green-700", "cursor-pointer");
                 } else {
-                    // ปิดใช้งานปุ่ม
                     document.getElementById("saveButton").disabled = true;
                     document.getElementById("saveButton").classList.remove("bg-green-700", "cursor-pointer");
                     document.getElementById("saveButton").classList.add("cursor-not-allowed", "bg-gray-500");
                 }
             }
+
+            function toggleError(fieldId, isValid, errorId = null) {
+                const input = document.getElementById(fieldId);
+                const errorSpan = document.getElementById(errorId || `${fieldId}-error`);
+
+                if (!isValid) {
+                    input.classList.add("error-input-style");
+                    errorSpan.classList.remove("hidden");
+                } else {
+                    input.classList.remove("error-input-style");
+                    errorSpan.classList.add("hidden");
+                }
+            }
         </script>
+
+
 
         @if (session('success'))
             <script>
