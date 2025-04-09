ce6b212c29d8f175cfa4d2b992e505ca05cbd02a
jeng <189732922+TorlapSayHi@users.noreply.github.com>
2025-04-09 09:30:44 +0000

    Add debounce to search input for improved performance

--- resources/views/poi/index.blade.php
+++ resources/views/poi/index.blade.php
@@ -67,13 +67,17 @@
         let pois = [];
         let currentPage = 1;
         const rowsPerPage = 10;
-
+        let searchTimeout;
         document.addEventListener("DOMContentLoaded", () => {
             fetchPois();
 
             document.getElementById("searchInput").addEventListener("input", function () {
-                currentPage = 1;
-                fetchPois(this.value);
+                clearTimeout(searchTimeout);
+                const search = this.value;
+                searchTimeout = setTimeout(() => {
+                    currentPage = 1;
+                    fetchPois(search);
+                }, 300); // หน่วงเวลา 300 มิลลิวินาที
             });
             
         });
